0eeea4e72e11acd74095cb1d970c5783c2bba473
Mathias Munk <mh@morningtrain.dk>
2023-10-02 14:39:32 +0200

    Fix global return type

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,11 +2,8 @@
 
 namespace MorningMedley\View;
 
-use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 use Illuminate\View\ViewServiceProvider;
-use Symfony\Component\Finder\Finder;
 
 class ServiceProvider extends IlluminateServiceProvider
 {
@@ -14,14 +11,14 @@ class ServiceProvider extends IlluminateServiceProvider
     {
         $this->mergeConfigFrom(__DIR__ . "/config/config.php", 'view');
         $this->app->register(ViewServiceProvider::class);
-        
+
         require_once __DIR__ . "/globals.php";
     }
 
     public function boot(): void
     {
         $this->app->get('config')
-            ->set('view.paths', $this->resolveRelativePaths($this->app->get('config')->get('view.paths')));
+            ->set('view.paths', $this->resolveRelativePaths((array)$this->app->get('config')->get('view.paths')));
         $this->app->get('config')
             ->set('view.compiled',
                 $this->resolveRelativePaths((array) $this->app->get('config')->get('view.compiled'))[0]);
--- src/globals.php
+++ src/globals.php
@@ -1,13 +1,15 @@
 <?php
 
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Facades\View as ViewFacade;
+use Illuminate\View\View;
 
 if (! function_exists('view')) {
     function view(
         string $view,
         Arrayable|array $data = [],
         array $mergeData = []
-    ): string {
-        return \Illuminate\Support\Facades\View::make($view, $data, $mergeData)->render();
+    ): View {
+        return ViewFacade::make($view, $data, $mergeData);
     }
 }
